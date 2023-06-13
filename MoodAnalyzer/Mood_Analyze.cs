32377045b925b691d917930afa7ff21702a29914
Vedant <vedantnp200@gmail.com>
2023-06-13 23:35:41 +0530

    Analyse happy and sad mood

---
+++ MoodAnalyzer/Mood_Analyze.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    internal class Mood_Analyze
+    {
+        public string HappyMood = "Happy";
+        public string SadMood = "Sad";
+
+        public string AnalyzeMood(string mood)
+        {
+            if (mood.Contains("sad"))
+            {
+                return SadMood;
+            }
+            else
+            {
+                return HappyMood;
+            }
+        }
+    }
+}
---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mood_Analyze mood = new Mood_Analyze();
+        }
+    }
+}
