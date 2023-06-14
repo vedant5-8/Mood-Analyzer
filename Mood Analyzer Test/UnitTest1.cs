aab6dfe4cafc075b1910fc704f7475719e105665
Vedant <vedantnp200@gmail.com>
2023-06-14 10:58:06 +0530

    [Vedant]-Analyse sad mood using constructor

--- Mood Analyzer Test/UnitTest1.cs
+++ Mood Analyzer Test/UnitTest1.cs
@@ -5,36 +5,19 @@ namespace Mood_Analyzer_Test
     [TestClass]
     public class UnitTest1
     {
-        /*
+        
         [TestMethod]
         public void TestCase1()
         {
-            Mood_Analyze mood = new Mood_Analyze();
-
             string message = "I am in sad mood.";
 
-            string ExpectedResult = mood.AnalyzeMood(message);
+            Mood_Analyze mood = new Mood_Analyze(message);
+
+            string ExpectedResult = mood.AnalyzeMood();
             string ActualResult = "Sad";
 
             Assert.AreEqual(ExpectedResult, ActualResult);
         }
-        */
-
-        /*
-        [TestMethod]
-        public void TestCase2()
-        {
-            Mood_Analyze mood = new Mood_Analyze();
-
-            string message = "I am in any mood";
-
-            string ExpectedResult = mood.AnalyzeMood(message);
-            string ActualResult = "Happy";
-
-            Assert.AreEqual(ExpectedResult, ActualResult);
-        }
-        */
-
 
     }
 }
