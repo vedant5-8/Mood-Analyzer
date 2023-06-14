1bbe52e7f4fcfc60c7ff15c9b8ee1be0bc88ab55
Vedant <vedantnp200@gmail.com>
2023-06-14 08:13:03 +0530

    Analyze sad mood

--- Mood Analyzer Test/UnitTest1.cs
+++ Mood Analyzer Test/UnitTest1.cs
@@ -17,5 +17,18 @@ namespace Mood_Analyzer_Test
 
             Assert.AreEqual(ExpectedResult, ActualResult);
         }
+
+        [TestMethod]
+        public void TestCase2()
+        {
+            Mood_Analyze mood = new Mood_Analyze();
+
+            string message = "I am in any mood";
+
+            string ExpectedResult = mood.AnalyzeMood(message);
+            string ActualResult = "Happy";
+
+            Assert.AreEqual(ExpectedResult, ActualResult);
+        }
     }
 }
