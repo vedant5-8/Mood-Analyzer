cf408d4610b6dbdc305df0943c1eb3e4e1b6c4ec
Vedant <vedantnp200@gmail.com>
2023-06-16 23:44:03 +0530

    [Vedant]-Inform user if entered Invalid Mood

---
+++ MoodAnalyzer/MoodAnalysisException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalysisException : Exception
+    {
+        public MoodAnalysisException() { }
+        public MoodAnalysisException(string message) : base(message)
+        {
+
+        }
+    }
+}
