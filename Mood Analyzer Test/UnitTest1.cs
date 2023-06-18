1fad9910bb73352867491e3947fa09bdd8991fea
Vedant <vedantnp200@gmail.com>
2023-06-18 15:19:56 +0530

    [Vedant]-Use Reflection to Create MoodAnalyser with parameterized Constructor

--- Mood Analyzer Test/UnitTest1.cs
+++ Mood Analyzer Test/UnitTest1.cs
@@ -56,5 +56,55 @@ namespace Mood_Analyzer_Test
                 Assert.AreEqual("Constructor not found", ex.Message);
             }
         }
+
+        // TC5.1 - Given MoodAnalyser When Proper Return MoodAnalyser Object
+
+        [TestMethod]
+        public void TestCase4()
+        {
+            try
+            {
+                object expected = new Mood_Analyze("I am in sad mood");
+                object actual = MoodAnalyserFactory.ReflectionForParameterizedConstructor("MoodAnalyzer.Mood_Analyze", "Mood_Analyze"); ;
+                expected.Equals(actual);
+            }
+            catch (CustomMoodAnalysisException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // TC5.2 - Given Class Name When Improper Should Throw CustomMoodAnalysisException
+
+        [TestMethod]
+        public void TestCase5()
+        {
+            try
+            {
+                object expected = new Mood_Analyze("I am in happy mood.");
+                object actual = MoodAnalyserFactory.ReflectionForParameterizedConstructor("MoodAnalyzer.Mood_Analyze1", "Mood_Analyze"); ;
+            }
+            catch (CustomMoodAnalysisException ex)
+            {
+                Assert.AreEqual("Class not found", ex.Message);
+            }
+        }
+
+        // TC5.3 - Given Class When Constructor Not Proper Should Throw MoodAnalysisException
+
+        [TestMethod]
+        public void TestCase6()
+        {
+            try
+            {
+                object expected = new Mood_Analyze("I am in any mood.");
+                object actual = MoodAnalyserFactory.ReflectionForParameterizedConstructor("MoodAnalyzer.Mood_Analyze", "Mood_Analyze1"); ;
+            }
+            catch (CustomMoodAnalysisException ex)
+            {
+                Assert.AreEqual("Constructor not found", ex.Message);
+            }
+        }
+
     }
 }
--- MoodAnalyzer/MoodAnalyserFactory.cs
+++ MoodAnalyzer/MoodAnalyserFactory.cs
@@ -28,5 +28,31 @@ namespace MoodAnalyzer
                 throw new CustomMoodAnalysisException(CustomMoodAnalysisException.MoodType.NO_SUCH_CLASS, "Class not found");
             }
         }
+
+        public static object ReflectionForParameterizedConstructor(string NameSapceAndclassName, string constructorName)
+        {
+
+            Type type = typeof(Mood_Analyze);
+
+            if (type.Name.Equals(NameSapceAndclassName) || type.FullName.Equals(NameSapceAndclassName))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { "" });
+                    return instance;
+                }
+                else
+                {
+                    throw new CustomMoodAnalysisException(CustomMoodAnalysisException.MoodType.NO_SUCH_METHOD, "Constructor not found");
+                }
+            }
+            else
+            {
+                throw new CustomMoodAnalysisException(CustomMoodAnalysisException.MoodType.NO_SUCH_CLASS, "Class not found");
+            }
+
+        }
+
     }
 }
